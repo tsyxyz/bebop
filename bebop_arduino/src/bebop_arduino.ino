df2daf19ec2d6e2d044cd9300195d2af0b92fe1e
tsyxyz <tsyxyz@hotmail.com>
2020-07-18 17:23:57 +0800

    bebop_arduino add sub chassis cmd

--- bebop_arduino/src/bebop_arduino.ino
+++ bebop_arduino/src/bebop_arduino.ino
@@ -2,6 +2,8 @@
 // Response to RC remote controller reference to https://www.geek-workshop.com/thread-2408-1-1.html
 
 #include <Servo.h>
+#include <ros.h>
+#include <bebop_control_msgs/ChassisCmd.h>
 
 #define SERVO_PIN 9
 #define ESC_PIN   10
@@ -12,25 +14,31 @@ int potpin = 0;
 int servo_val;
 int esc_val;
 
+void OnChassisCmd(const bebop_control_msgs::ChassisCmd& cmd) {
+    servo_val = map(cmd.steer, 
+                    bebop_control_msgs::ChassisCmd::MIN_STEER,
+                    bebop_control_msgs::ChassisCmd::MAX_STEER,
+                    0, 180);
+    esc_val = map(cmd.velocity,
+                  bebop_control_msgs::ChassisCmd::MIN_VELOCITY,
+                  bebop_control_msgs::ChassisCmd::MAX_VELOCITY,
+                  1000, 2000);
+}
+
+ros::NodeHandle nh;
+ros::Subscriber<bebop_control_msgs::ChassisCmd> cmd_sub("/chassis_cmd", &OnChassisCmd);
+
 void setup() {
+    nh.initNode();
+    nh.subscribe(cmd_sub);
+
     servo.attach(SERVO_PIN);
 
     esc.attach(ESC_PIN);
     esc.writeMicroseconds(1000); // initialize the signal to 1000
-
-    Serial.begin(9600);
 }
 
 void loop() {
-    servo_val = analogRead(potpin);
-    servo_val = map(servo_val, 0, 1023, 0, 180);
-    Serial.println(servo_val);
     servo.write(servo_val);
-
-    esc_val = analogRead(A0);
-    esc_val = map(esc_val, 0, 1023, 1000, 2000);
-    Serial.println(esc_val);
     esc.writeMicroseconds(esc_val);
-
-    // delay(15);
 }
