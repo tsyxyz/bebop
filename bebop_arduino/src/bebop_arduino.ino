f937b76af32a24564cdfa621b562c4c0a3a1af2e
tsyxyz <tsyxyz@hotmail.com>
2020-09-24 23:47:25 +0800

    rename const values

--- bebop_arduino/src/bebop_arduino.ino
+++ bebop_arduino/src/bebop_arduino.ino
@@ -5,37 +5,43 @@
 #include <ros.h>
 #include <bebop_control_msgs/ChassisCmd.h>
 
-#define SERVO_PIN 9
-#define ESC_PIN   10
+const int kServoPin = 9;
+const int kEscPin = 10;
+const char* kChassisCmdTopic = "/chassis_cmd";
+const int kServoMiddle = 90;
+const int kEscMiddle = 1500;
 
 Servo servo;
 Servo esc;
-int potpin = 0;
 int servo_val;
 int esc_val;
 
 void OnChassisCmd(const bebop_control_msgs::ChassisCmd& cmd) {
     servo_val = map(cmd.steer, 
-                    bebop_control_msgs::ChassisCmd::MIN_STEER,
-                    bebop_control_msgs::ChassisCmd::MAX_STEER,
+                    bebop_control_msgs::ChassisCmd::kMinSteer,
+                    bebop_control_msgs::ChassisCmd::kMaxSteer,
                     0, 180);
     esc_val = map(cmd.throttle,
-                  bebop_control_msgs::ChassisCmd::MIN_THROTTLE,
-                  bebop_control_msgs::ChassisCmd::MAX_THROTTLE,
+                  bebop_control_msgs::ChassisCmd::kMinThrottle,
+                  bebop_control_msgs::ChassisCmd::kMaxThrottle,
                   1000, 2000);
 }
 
 ros::NodeHandle nh;
-ros::Subscriber<bebop_control_msgs::ChassisCmd> cmd_sub("/chassis_cmd", &OnChassisCmd);
+ros::Subscriber<bebop_control_msgs::ChassisCmd> cmd_sub(kChassisCmdTopic, &OnChassisCmd);
 
 void setup() {
+    servo_val = kServoMiddle;
+    esc_val = kEscMiddle;
+
     nh.initNode();
     nh.subscribe(cmd_sub);
 
-    servo.attach(SERVO_PIN);
+    servo.attach(kServoPin);
+    servo.write(kServoMiddle);
 
-    esc.attach(ESC_PIN);
-    esc.writeMicroseconds(1000); // initialize the signal to 1000
+    esc.attach(kEscPin);
+    esc.writeMicroseconds(kEscMiddle);
 }
 
 void loop() {
